10fda0a43fb35040ccb2d115ea50da7825debe56
szymio11 <szymio11@gmail.com>
2018-03-21 22:29:46 +0100

    test
    test

--- InterOn/InterOn.Api/Startup.cs
+++ InterOn/InterOn.Api/Startup.cs
@@ -69,7 +69,7 @@ namespace InterOn.Api
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddScoped<IRoleService, RoleService>();
         }
-
+     
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
--- InterOn/InterOn.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace InterOn.Data
-{
-    public class Class1
-    {
-    }
-}
